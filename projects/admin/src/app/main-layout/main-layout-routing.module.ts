992c175ec88b07cd80b086d877285ef8eaccde99
MohamedShaban977 <shban977@gmail.com>
2023-10-13 23:45:22 +0300

    handle route with navbar

--- projects/admin/src/app/main-layout/main-layout-routing.module.ts
+++ projects/admin/src/app/main-layout/main-layout-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tasks',
+    loadChildren: () => import(`./tasks-admin/tasks-admin.module`).then(m => m.TasksAdminModule)
+  },
+  {
+    path: 'users',
+    loadChildren: () => import(`./manage-users/manage-users.module`).then(m => m.ManageUsersModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
